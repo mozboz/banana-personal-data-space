648f5601fe0c5a57b8d8e67f5b90ef22bcdacc73
mozboz <lewis.james@gmail.com>
2014-06-29 19:43:20 +0200

    profile context and tests

--- src/test/java/modelTest.scala
+++ src/test/java/modelTest.scala
@@ -1,7 +1,7 @@
 import collection.mutable.Stack
 import org.scalatest._
 
-class newTest2 extends FlatSpec with Matchers {
+class modelTest extends FlatSpec with Matchers {
 
   "A Stack" should "pop values in last-in-first-out order" in {
     val stack = new Stack[Int]
