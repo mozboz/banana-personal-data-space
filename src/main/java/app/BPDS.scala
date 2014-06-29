f785184c2b458a6444ae9f1260657fdb7c6242e7
janesen <daniel@djanz.de>
2014-06-29 18:42:54 +0200

    Endpoint actor

--- src/main/java/app/BPDS.scala
+++ src/main/java/app/BPDS.scala
@@ -1,21 +1,57 @@
 package app
+import akka.actor.{Actor, ActorSystem, Props}
 
-import akka.actor.{Props, ActorSystem}
-import actors._
-import deleteme.Guessable
+trait Action
+trait Thing
+
+trait UrlAdressable {
+  def getUrl = ""
+}
+
+trait Identifyable {
+  def getId = ""
+}
+
+trait Store
+
+case class Profile(url : String)
+  extends Store
+  with    UrlAdressable {
+  override def getUrl = url
+}
+
+case class Context(id : String)
+  extends Thing
+  with    Store
+  with    Identifyable {
+  override def getId = id
+}
+
+case class Item(id : String)
+  extends Thing
+  with    Identifyable{
+  override def getId = id
+}
+
+case class Add   (val item : Thing with Identifyable, val to   : Store)  extends Action
+case class Update(val item : Thing with Identifyable, val in   : Store)  extends Action
+case class Remove(val item : Thing with Identifyable, val from : Store)  extends Action
+
+
+class EndpointActor extends Actor {
+  def receive = {
+    case Add(item:Context,to:Profile) => throw new Exception("Let it crash");
+    case Add(item:Item,to:Context) => throw new Exception("Let it crash");
+    case x => throw new Exception("Unknown message!");
+  }
+}
 
 class BPDS extends App {
 
-  val system = ActorSystem("YoDawgIHeardYouLikeActorsSoIPutSomeActorsInYourActors")
-  val masterActor = system.actorOf(Props[MasterActor], name = "MasterActor")  // the local actor
-  masterActor ! Action.ADD
 
-  /*
-  def receive = {
-    case msg: String =>
-      println(msg)
-      // sender ! "Hello from the RemoteActor"
-    // case _ => null
-  }
-    */
+  implicit val system = ActorSystem("Profile system")
+  val endpoint = system.actorOf(Props[EndpointActor], name = "EndpointActor")  // the local actor
+
+  endpoint !  Add(item=Context("context name"), to=Profile("http://profile.daniel.de"))
+  endpoint !  Add(item=Item("item name"), to=Context("context name"))
 }
