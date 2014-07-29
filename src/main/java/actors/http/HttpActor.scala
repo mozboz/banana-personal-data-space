c50e81d76aa2b4a0dbbcfe86a3f42368617b99bc
mozboz <lewis.james@gmail.com>
2014-07-29 05:13:36 +0200

    working http

---
+++ src/main/java/actors/http/HttpActor.scala
@@ -0,0 +1,12 @@
+package actors.http
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: james
+ * Date: 29/07/14
+ * Time: 03:06
+ * To change this template use File | Settings | File Templates.
+ */
+class HttpActor {
+
+}
---
+++ src/main/java/requests/HTTP.scala
@@ -0,0 +1,12 @@
+package requests
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: james
+ * Date: 29/07/14
+ * Time: 03:54
+ * To change this template use File | Settings | File Templates.
+ */
+class HTTP {
+
+}
