8ca233bc0b99d98cb6c79f18428a40d56cd158a2
mozboz <lewis.james@gmail.com>
2014-07-30 15:12:01 +0200

    partial commit of url/http stuff

--- src/main/java/utils/RequestStringParser.scala
+++ src/main/java/utils/RequestStringParser.scala
@@ -1,5 +1,22 @@
 package utils
 
-object RequestStringParser {
+import actors.behaviors.Request
+import scala.util.matching.Regex
+import requests.Read
 
+object RequestStringParser {
+  def parseUrl(requestString: String) = parse(requestString.stripPrefix("/"))
+  def parse(requestString: String) = {
+    val keyOnly = "([^\\s\\\\]+)".r
+    val keyAndContext = "([^\\s\\\\]+)\\/([)^\\s\\\\]+)".r
+
+    requestString match {
+      case keyAndContext(key, context) => Read(key, context)
+      case keyOnly(context) => Read(null, context)
+
+      case _ => throw new IllegalArgumentException("Poorly formatted Request String")
+    }
+
+
+  }
 }
--- src/test/java/RequestStringParser.scala
+++ src/test/java/RequestStringParser.scala
@@ -1,10 +1,12 @@
 import org.scalatest._
+import utils.RequestStringParser
 
 class RequestStringParser extends FlatSpec with Matchers {
 
     "a request string" should "parse into correct components" in {
 
-      "/foo".stripPrefix("/") should be ("foo")
+      // "/foo".stripPrefix("/") should be ("foo")
+      RequestStringParser.parse("foo/bar") should be ("foobar")
     }
 
     it should "do some other stuff" in {
